531e1601170e7b7123ac815ef294f2109e75eca9
Derek.Browne <korvanica@gmail.com>
2017-01-20 21:38:53 +0000

    Player Controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour {
 
     public float moveSpeed = 5;
+    PlayerController controller;
 
 	// Use this for initialization
 	void Start () {
-		
+        controller = GetComponent<PlayerController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rig;
+    Vector3 vel;
+
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rig.MovePosition(rig.position + vel * Time.fixedDeltaTime);
+	}
+
+    public void Move(Vector3 _vel) {
+        vel = _vel;
+    }
+}
