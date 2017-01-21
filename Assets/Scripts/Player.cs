32c26505f9e237bb3743774df834262f40953725
Derek.Browne <korvanica@gmail.com>
2017-01-21 14:27:44 +0000

    Scene Change, Input Change

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Player : LivingEntity {
 
     public float moveSpeed = 5;
+    public float pulseCooldown = 5;
+    public float nextPulseAvailableTime;
+
     PlayerController controller;
 
     // Use this for initialization
@@ -13,10 +16,12 @@ public class Player : LivingEntity {
     {
         base.Start();
         controller = GetComponent<PlayerController>();
+        nextPulseAvailableTime = Time.timeSinceLevelLoad;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //Movement Input
         bool rotate = false;
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
@@ -25,5 +30,15 @@ public class Player : LivingEntity {
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity, rotate);
+
+        //Pulse Input
+        if (Input.GetAxis("Jump") != 0)
+        {
+            if (Time.timeSinceLevelLoad > nextPulseAvailableTime)
+            {
+                nextPulseAvailableTime = Time.timeSinceLevelLoad + pulseCooldown;
+                print("Jump");
+            }
+        }
     }
 }
