5cf8c4aa3087e480fe1fd32220c6ceff4c7d5489
Derek.Browne <korvanica@gmail.com>
2017-01-21 15:18:28 +0000

    Jump to Pulse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : LivingEntity {
 
     PlayerController controller;
 
+    public event System.Action OnDeath;
+    public event System.Action OnTriggerPulse;
     // Use this for initialization
     public override void Start()
     {
@@ -37,7 +39,7 @@ public class Player : LivingEntity {
             if (Time.timeSinceLevelLoad > nextPulseAvailableTime)
             {
                 nextPulseAvailableTime = Time.timeSinceLevelLoad + pulseCooldown;
-                print("Jump");
+                print("Pulse");
             }
         }
     }
