170b67658e3bf06ebcde4a0c4d42c00833c113d5
Chris Harrington <chris.harrington@retailinmotion.com>
2017-01-21 10:39:26 +0000

    added obstacle generation

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -6,4 +6,19 @@ public static class Utility {
     {
         return "Test";
     }
+
+	public static T[] ShuffleArray<T>(T[] array, int seed) {
+
+		System.Random rand = new System.Random (seed);
+
+		for (int i = 0; i < array.Length - 1; i++) {
+
+			int randomIndex = rand.Next (i, array.Length);
+			T temp = array [randomIndex];
+			array [randomIndex] = array [i];
+			array [i] = temp;
+		}
+
+		return array;
+	}
 }
