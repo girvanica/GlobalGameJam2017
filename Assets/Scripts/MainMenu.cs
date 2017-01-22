81b06b43b2345b3e18a8b304c364387322ccf720
Steve <stephenbyrne91@gmail.com>
2017-01-22 12:03:27 +0000

    Xbox Controller support for MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,19 +7,44 @@ public class MainMenu : MonoBehaviour {
 	public Canvas quitMenu;
 	public Button startText;
 	public Button exitText;
+    UnityEngine.UI.Button yesButton;
+    UnityEngine.UI.Button noButton;
+    bool controllerConnected;
 
-	void Start () {
-		quitMenu = quitMenu.GetComponent<Canvas> ();
-		startText = startText.GetComponent<Button> ();
-		exitText = exitText.GetComponent<Button> ();
-		quitMenu.enabled = false;
-	}
+    void Start()
+    {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
 
+        string[] names = Input.GetJoystickNames();
+
+        controllerConnected = false;
+
+        for (int x = 0; x < names.Length; x++)
+        {
+            if (names[x].ToUpper().Contains("XBOX")){        
+                controllerConnected = true;
+                startText.enabled = true;
+                startText.Select();
+            }
+        }
+    }
+   
 	public void ExitPress() {
-		quitMenu.enabled = true;
+        quitMenu.enabled = true;
 		startText.enabled = false;
 		exitText.enabled = false;
-	}
+        if (controllerConnected && Input.GetButtonDown("Jump"))
+        {
+            quitMenu.gameObject.SetActive(true);
+            quitMenu.enabled = true;
+            yesButton = GameObject.Find("Yes").GetComponent<UnityEngine.UI.Button>();
+            noButton = GameObject.Find("No").GetComponent<UnityEngine.UI.Button>();
+            yesButton.Select();
+        }
+    }
 
 	public void NoPress() {
 		quitMenu.enabled = false;
