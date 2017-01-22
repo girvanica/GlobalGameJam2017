7a362ec2ee612377cb58a698dd1d5e730cb24685
Derek.Browne <korvanica@gmail.com>
2017-01-22 13:09:01 +0000

    Decoy Pickup

---
+++ Assets/Scripts/Decoy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decoy : MonoBehaviour
+{ 
+    public event System.Action OnDecoyPickup;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            // Play sound
+            TriggerDecoyPickUp();
+            Destroy(gameObject);
+        }
+    }
+
+    void TriggerDecoyPickUp()
+    {
+        if (OnDecoyPickup != null)
+        {
+            OnDecoyPickup();
+        }
+    }
+}
